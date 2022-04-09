11e21fbfa59c1ad293a66ecdc1b31ea731ddfca2
Ritwik Chawla <73139315+ritwikchawla@users.noreply.github.com>
2022-04-09 12:28:03 +0530

    Create Array Functions.js

---
+++ Array Functions.js
@@ -0,0 +1,16 @@
+// Progression #1: Greatest of the two
+function greatestoftwonumbers(n1,n2){
+    return Math.max(n1,n2);
+}
+
+var result = greatestoftwonumbers(10,20);
+console.log(result);
+
+// Progression #2: The lengthy word
+var size = 0;
+function scaryWord(words){
+    for(let i=0;i<words.length;i++){
+        words[i].length
+    }
+    
+}
